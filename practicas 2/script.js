aa765f31cc857354cb8f2e54088005d336a19918
Torrescerv <coretorrescerv@gmail.com>
2024-09-18 10:01:17 -0500

    Add files via upload

---
+++ practicas 2/script.js
@@ -0,0 +1,42 @@
+// 9.1 Comentarios 
+console.log("ejemplos de javascript estan en la consola.");
+
+let x =10;
+let y = 5;
+console.log(x + y);
+
+let suma = x + y;
+let resta = x - y;
+let multiplicacion = x * y;
+let division = x / y;
+console.log("suma:", suma);
+console.log("resta:", resta);
+console.log("multiplicación:", multiplicacion);
+console.log("Division:", multiplicacion);
+console.log("division:", division);
+
+// 9.7 JavaScript es case Sensitive
+let nombre = "Juan";
+let Nombre = "Pedro";
+console.log("nombre en minúscula:", nombre);
+console.log("Nombre en mayuscula;", Nombre);
+
+// 9.8 Tipos de Datos
+let esVerdadero = true; // Booleano
+let numero = 100; // Numero
+let texto = "Hola, mundo"; // String
+
+console.log("Booleano:", esVerdadero);
+console.log("Número:", numero);
+console.log("String:", texto);
+
+//9.10 Números
+let numeroEntero = 10; // Numero entero
+let numeroDecimal = 10.3; // Numero decimal
+
+console.log("numero entero:", numeroEntero);
+console.log("numero decimal:", numeroDecimal);
+console.log("numeroDecimal*numeroEntero");
+
+
+
